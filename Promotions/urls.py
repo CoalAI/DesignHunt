bd9982e1fd8851a86a7c5cf04859464c1d4b7e59
Muhammad Kamran <mkrnaqeebi@gmail.com>
2020-01-23 13:34:58 +0500

    added fluke 8 pre commit and pytest and many more

--- Promotions/urls.py
+++ Promotions/urls.py
@@ -5,9 +5,6 @@ from django.urls import path
 
 from Promotions.views import PromotionViewSet
 
-# from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-
-
 urlpatterns = [
     path('promotion/', PromotionViewSet.as_view()),
 ]
