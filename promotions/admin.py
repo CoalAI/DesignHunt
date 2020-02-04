31244c2cb61ea6dd38412c9002cafe041eeb8f85
Muhammad Kamran <mkrnaqeebi@gmail.com>
2020-02-04 18:00:00 +0500

    admin panal gets mail gun and user

--- promotions/admin.py
+++ promotions/admin.py
@@ -2,12 +2,24 @@
 RestInterface.admin
 """
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from promotions.models import Promotion
+from authentication.models import User
+from promotions.models import MailGun
 
 admin.site.site_header = _('Design Hunt')
 admin.site.site_title = _('Design Hunt')
 
 
-admin.site.register(Promotion)
+class CustomUserAdmin(UserAdmin):
+    """
+    To added extra fields and prieous fields togather
+    """
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('contact_number',)}),
+    )
+
+
+admin.site.register(MailGun)
+admin.site.register(User, CustomUserAdmin)
